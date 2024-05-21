3495e65f30e826bf76919c53c61ea31486f70ef8
root <sowmyakancharla2424@gmail.com>
2024-05-21 09:51:22 +0000

    react

--- src/Components/DesignedBy/index.js
+++ src/Components/DesignedBy/index.js
@@ -1,36 +1,39 @@
 import {FaLinkedin, FaGithubSquare} from 'react-icons/fa'
 
 import {FiExternalLink} from 'react-icons/fi'
-
+import Header from '../Header'
 import './index.css'
 
 const DesignedBy = () => (
-  <div className="my-container">
-    <div className="border-con">
-      <h1 className="my-heading">About me</h1>
-      <div className="name-con">
-        <p className="text">Name:Sowmya Sri Kancharla</p>
-      </div>
-      <div>
-        <p className="text">
-          LinkedIn <FaLinkedin /> :{' '}
-          <a
-            className="link"
-            href="https://www.linkedin.com/in/sowmya-sri-kancharla-47569a299/"
-          >
-            www.linkedin.com
-            <FiExternalLink className="visit-icons" />
-          </a>
-        </p>
-      </div>
-      <div>
-        <p className="text">
-          Github <FaGithubSquare /> :{' '}
-          <a className="link" href="https://github.com/sowmyasrikancharla/">
-            www.Github.com
-            <FiExternalLink className="visit-icons" />
-          </a>
-        </p>
+  <div>
+    <Header />
+    <div className="my-container">
+      <div className="border-con">
+        <h1 className="my-heading">About me</h1>
+        <div className="name-con">
+          <p className="text">Name:Sowmya Sri Kancharla</p>
+        </div>
+        <div>
+          <p className="text">
+            LinkedIn <FaLinkedin /> :{' '}
+            <a
+              className="link"
+              href="https://www.linkedin.com/in/sowmya-sri-kancharla-47569a299/"
+            >
+              www.linkedin.com
+              <FiExternalLink className="visit-icons" />
+            </a>
+          </p>
+        </div>
+        <div>
+          <p className="text">
+            Github <FaGithubSquare /> :{' '}
+            <a className="link" href="https://github.com/sowmyasrikancharla/">
+              www.Github.com
+              <FiExternalLink className="visit-icons" />
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   </div>
